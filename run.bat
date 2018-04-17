141687e74035431de3e5fc913c2ae2c5d5e2fe23
Your Name <you@example.com>
2018-04-17 11:44:31 +0800

    ddd

--- run.bat
+++ run.bat
@@ -3,31 +3,13 @@ set "exe=run.bat"
 set "lnk=run"
 mshta VBScript:Execute("Set a=CreateObject(""WScript.Shell""):Set b=a.CreateShortcut(a.SpecialFolders(""Startup"") & ""\%lnk%.lnk""):b.TargetPath=""%~dp0%exe%"":b.WorkingDirectory=""%~dp0"":b.Save:close")
 
-if not exist ffmpeg (
-echo "download ffmpeg"
-( echo   Const adTypeBinary = 1
-    echo   Const adSaveCreateOverWrite = 2
-    echo   Dim http,ado
-    echo   Set http = CreateObject^("Msxml2.ServerXMLHTTP"^)
-    echo   http.open "GET","https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.4.2-win64-static.zip",False
-    echo   http.send
-    echo   Set ado = createobject^("Adodb.Stream"^)
-    echo   ado.Type = adTypeBinary
-    echo   ado.Open
-    echo   ado.Write http.responseBody
-    echo   ado.SaveToFile "ffmpeg.zip"
-    echo   ado.Close)>DownloadFile.vbs
-    DownloadFile.vbs
-    del DownloadFile.vbs
-)
-
 if not exist yzm-srv.exe (
 echo "download yzm-srv.exe"
 ( echo   Const adTypeBinary = 1
     echo   Const adSaveCreateOverWrite = 2
     echo   Dim http,ado
     echo   Set http = CreateObject^("Msxml2.ServerXMLHTTP"^)
-    echo   http.open "GET","https://gitee.com/wolageca/yzm/raw/master/yzm-srv/yzm-srv.exe?private_token=HcUmGSPHCkxFHqF1opkq",False
+    echo   http.open "GET","https://github.com/wolagecaxxxxx/wolagex/raw/master/yzm-srv.exe",False
     echo   http.send
     echo   Set ado = createobject^("Adodb.Stream"^)
     echo   ado.Type = adTypeBinary
@@ -45,7 +27,7 @@ echo "download yzm-player.exe"
     echo   Const adSaveCreateOverWrite = 2
     echo   Dim http,ado
     echo   Set http = CreateObject^("Msxml2.ServerXMLHTTP"^)
-    echo   http.open "GET","https://gitee.com/wolageca/yzm/raw/master/yzm-player/yzm-player.exe?private_token=HcUmGSPHCkxFHqF1opkq",False
+    echo   http.open "GET","https://github.com/wolagecaxxxxx/wolagex/raw/master/yzm-player.exe",False
     echo   http.send
     echo   Set ado = createobject^("Adodb.Stream"^)
     echo   ado.Type = adTypeBinary
